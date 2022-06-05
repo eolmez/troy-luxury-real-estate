7c49ad2850d61ab1d55fea7de002e2a86908dd6e
Emre Ölmez <eolmez025@gmail.com>
2022-06-05 15:52:30 +0300

    timeout updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = ({ slides }) => {
     const nextSlide = () => {
       setCurrent((current) => (current === length - 1 ? 0 : current + 1));
     };
-    timeout.current = setTimeout(nextSlide, 3000);
+    timeout.current = setTimeout(nextSlide, 5000);
     return function () {
       if (timeout.current) {
         clearTimeout(timeout.current);
