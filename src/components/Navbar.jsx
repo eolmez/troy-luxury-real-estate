2351a91d31048364445829c3fe655ed5299d7659
Emre Ölmez <eolmez025@gmail.com>
2022-06-05 15:49:25 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,109 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { css } from "styled-components/macro";
+import { menuData } from "../data/MenuData";
+import { Button } from "./Button";
+import { FaHorseHead, FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons";
+
+const Navbar = ({ toggle }) => {
+  return (
+    <Nav>
+      <Logo to="/">
+        <IconContext.Provider value={{size: 28}}>
+          <NavIcon />
+        </IconContext.Provider>
+        TROY
+      </Logo>
+      <MenuBars onClick={toggle} />
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+      <NavBtn>
+        <Button to="/contact" primary="true">
+          Contact Us
+        </Button>
+      </NavBtn>
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+`;
+
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+const Logo = styled(Link)`
+  ${NavLink}
+  font-style: italic;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
+const NavIcon = styled(FaHorseHead)`
+  margin-right: 0.2rem;
+`;
+
+const MenuBars = styled(FaBars)`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    color: #fff;
+    @media screen and (max-width: 768px) {
+      display: block;
+      position: absolute;
+      top: 0;
+      right: 0;
+      transform: translate(-100%, 60%);
+      font-size: 1.5rem;
+      cursor: pointer;
+      color: #fff;
+    }
+  }
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+
+  @media screen and (max-width: 760px) {
+    display: none;
+  }
+`;
+
+const NavMenuLinks = styled(Link)`
+  ${NavLink}
+`;
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 760px) {
+    display: none;
+  }
+`;
+
+export default Navbar;
---
+++ src/data/InfoData.jsx
@@ -0,0 +1,26 @@
+import ImageOne from "../images/interior-1.jpg";
+import ImageTwo from "../images/house-5.jpg";
+
+export const InfoData = {
+  heading: "Explore our beautiful homes",
+  parapgraphOne:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat quas explicabo harum fuga tempora voluptas consequatur itaque cumque qui quisquam, optio laborum. Impedit enim quos quod cumque repudiandae pariatur facilis.",
+  paragraphTwo:
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint amet, modi, saepe commodi dolorum libero odit, laudantium labore porro cum veritatis nisi fugiat quisquam quas magnam assumenda eaque! Cum, fuga.",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoTwo = {
+  heading: "Modern Designs",
+  parapgraphOne:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat quas explicabo harum fuga tempora voluptas consequatur itaque cumque qui quisquam, optio laborum. Impedit enim quos quod cumque repudiandae pariatur facilis.",
+  paragraphTwo:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti neque possimus molestiae aut voluptate mollitia voluptatem magnam culpa enim excepturi!",
+  buttonLabel: "View Homes",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
